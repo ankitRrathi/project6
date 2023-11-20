cbeec1dff119af5a3b25a91c7fb6287da96aac3c
Ankit Rathi <ar487084@gmail.com>
2023-11-20 12:38:02 +0530

    day 1 app_icon_1024

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return const MaterialApp(
+    home: Homepage(),
+
+   );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const  Text('APi'),
+        centerTitle: true,
+      ), 
+    );
+  }
+}
